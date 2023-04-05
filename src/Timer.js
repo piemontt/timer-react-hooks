cc72c4b298019cd306d950fb04a591f23ce30602
Dmitry Pisanov <dima.pisanov@gmail.com>
2023-04-05 20:40:31 +0300

    v 1.0

--- src/Timer.js
+++ src/Timer.js
@@ -2,46 +2,47 @@ import React, { useState, useEffect } from 'react';
 
 export function Timer() {
             const localStorageTime = parseInt(localStorage.getItem("time"));
-            let isPaused = true;
-    const [count, setCount] = useState(localStorageTime || 0);
-            const [isMounted, setIsMounted] = useState(true);
+            const [count, setCount] = useState(localStorageTime || 0);
+            const [automatic, setAutomatic] = useState(false);
+            const [isActive, setIsActive] = useState(false);
 
             useEffect(() => {
-        
+                if (!automatic) return;
+
                     const interval = setInterval(() => {
-                        setCount(count => isPaused === false ? count + 1 : count);
+                        setCount(c => c + 1);
                     }, 1000);
 
                     return () => clearInterval(interval);
-            }, []);
+            }, [automatic]);
 
             const hour = count / 60 / 60 % 60;
             const minute = count / 60 % 60;
             const second = count % 60;
 	
             localStorage.setItem("time", JSON.stringify(count));
-            
 
+            const handleReset = function() {
+                localStorage.setItem("time", JSON.stringify(0));
+                setCount(c => c = 0)
+            };
 
-            const handleUnmount = () => {
-                    setIsMounted(false);
-            }
-            if (!isMounted) {
-                localStorage.setItem("time", '0');
-                return null;
-            }
+            const handleClick = event => {
+                setIsActive(current => !current);
+                setAutomatic(!automatic)
+            };
 
             return (
 
                 <div className="container-fluid mt-5 d-flex justify-content-center align-items-center flex-column">
-                    <span className="fs-1">{`${Math.trunc(hour)} hours : ${Math.trunc(minute)} minutes : ${Math.trunc(second)} seconds`}  left </span>
+                    <span className="fs-1">{`${Math.trunc(hour)} hours : ${Math.trunc(minute)} ${minute === 1 ? "minute" : "minutes"} : ${Math.trunc(second)} ${second === 1 ? "second" :"seconds"}`}  left </span>
                             <div className="buttons mt-5 d-flex ">
-                                <button type="button" className="btn btn-success d-block m-1" onClick={ () => isPaused = false }>
-                                    Go
-                                </button>
-                                <button type="button" className="btn btn-danger d-block m-1" onClick={ handleUnmount }>
-                                    Clear
-                                </button>
+                        <button type="button" className={isActive ? 'btn-danger btn btn-success d-block m-1' : 'btn-success btn btn-success d-block m-1'} onClick={handleClick}>
+                            turn {automatic ? 'off' : 'on'}
+                        </button>
+                        <button type="button" className="btn btn-secondary d-block m-1" onClick={handleReset}>
+                            reset
+                        </button>
                             </div>
                         </div>
                 )
