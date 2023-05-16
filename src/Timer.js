288e12e49320e13ca9651d2e45abd5738d5ab777
Dmitry Pisanov <dima.pisanov@gmail.com>
2023-05-16 23:24:58 +0300

    Что то переделал

--- src/Timer.js
+++ src/Timer.js
@@ -3,46 +3,43 @@ import React, { useState, useEffect } from 'react';
 export function Timer() {
             const localStorageTime = parseInt(localStorage.getItem("time"));
             const [count, setCount] = useState(localStorageTime || 0);
-            const [automatic, setAutomatic] = useState(false);
-            const [isActive, setIsActive] = useState(false);
+            const [isTurned, setTurned] = useState(false);
 
             useEffect(() => {
-                if (!automatic) return;
+                if (!isTurned) return;
 
                     const interval = setInterval(() => {
                         setCount(c => c + 1);
                     }, 1000);
 
                     return () => clearInterval(interval);
-            }, [automatic]);
+            }, [isTurned]); // Сам таймер
 
             const hour = count / 60 / 60 % 60;
             const minute = count / 60 % 60;
-            const second = count % 60;
+            const second = count % 60; // Делится на часы, секунды, минуты
 	
-            localStorage.setItem("time", JSON.stringify(count));
+            localStorage.setItem("time", JSON.stringify(count)); // Значение записывается в localStorage
 
-            const handleReset = function() {
-                localStorage.setItem("time", JSON.stringify(0));
-                setCount(c => c = 0)
-            };
+            const reset = function() {
+                localStorage.setItem("time", 0);
+                setCount(0);
+            }; // Обнуляет значение на странице и в localStorage
 
-            const handleClick = event => {
-                setIsActive(current => !current);
-                setAutomatic(!automatic)
-            };
+            const handleClick = function (event) {
+                setTurned(!isTurned)
+            }; // Функция, запускающая с кнопки таймер
 
             return (
-
                 <div className="container-fluid mt-5 d-flex justify-content-center align-items-center flex-column">
                     <span className="fs-1">{`${Math.trunc(hour)} hours : ${Math.trunc(minute)} ${minute === 1 ? "minute" : "minutes"} : ${Math.trunc(second)} ${second === 1 ? "second" :"seconds"}`}  left </span>
                             <div className="buttons mt-5 d-flex ">
-                        <button type="button" className={isActive ? 'btn-danger btn btn-success d-block m-1' : 'btn-success btn btn-success d-block m-1'} onClick={handleClick}>
-                            turn {automatic ? 'off' : 'on'}
-                        </button>
-                        <button type="button" className="btn btn-secondary d-block m-1" onClick={handleReset}>
-                            reset
-                        </button>
+                                <button type="button" className={isTurned ? 'btn-danger btn btn-success d-block m-1' : 'btn-success btn btn-success d-block m-1'} onClick={handleClick}>
+                                    turn {isTurned ? 'off' : 'on'}
+                                </button>
+                                <button type="button" className="btn btn-secondary d-block m-1" onClick={reset}>
+                                reset
+                                </button>
                             </div>
                         </div>
                 )
